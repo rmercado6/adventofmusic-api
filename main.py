3c044c718820c642c639cb354f5d4190fe9a52ed
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-11-30 17:34:19 +0000

    feat(db): connect to postgres db

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import psycopg
+from os import environ
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+
+config = {
+    'conninfo': environ['DATABASE_URL'],
+}
 
 
 @app.get("/")
@@ -9,143 +18,16 @@ def read_root():
 
 
 @app.get("/items")
-def read_item():
-    return [
-        {
-            "day": 1,
-            "class": "past",
-            "title": "Snowflake melody",
-            "body": "",
-            "emoji": ":notes:",
-            "tags": [
-                "melody",
-                "period form"
-            ]
-        },
-        {
-            "day": 2,
-            "class": "active",
-            "title": "Snowflake melody 2",
-            "emoji": ":notes:",
-            "tags": [
-                "melody",
-                "period form"
-            ]
-        },
-        {
-            "day": 3,
-            "class": "upcoming"
-        },
-        {
-            "day": 4,
-            "class": "upcoming"
-        },
-        {
-            "day": 5,
-            "class": "upcoming"
-        },
-        {
-            "day": 6,
-            "class": "upcoming"
-        },
-        {
-            "day": 7,
-            "class": "upcoming"
-        },
-        {
-            "day": 8,
-            "class": "upcoming"
-        },
-        {
-            "day": 9,
-            "class": "upcoming"
-        },
-        {
-            "day": 10,
-            "class": "upcoming"
-        },
-        {
-            "day": 11,
-            "class": "upcoming"
-        },
-        {
-            "day": 12,
-            "class": "upcoming"
-        },
-        {
-            "day": 13,
-            "class": "upcoming"
-        },
-        {
-            "day": 14,
-            "class": "upcoming"
-        },
-        {
-            "day": 15,
-            "class": "upcoming"
-        },
-        {
-            "day": 16,
-            "class": "upcoming"
-        },
-        {
-            "day": 17,
-            "class": "upcoming"
-        },
-        {
-            "day": 18,
-            "class": "upcoming"
-        },
-        {
-            "day": 19,
-            "class": "upcoming"
-        },
-        {
-            "day": 20,
-            "class": "upcoming"
-        },
-        {
-            "day": 21,
-            "class": "upcoming"
-        },
-        {
-            "day": 22,
-            "class": "upcoming"
-        },
-        {
-            "day": 23,
-            "class": "upcoming"
-        },
-        {
-            "day": 24,
-            "class": "upcoming"
-        },
-        {
-            "day": 25,
-            "class": "upcoming"
-        },
-        {
-            "day": 26,
-            "class": "disabled"
-        },
-        {
-            "day": 27,
-            "class": "disabled"
-        },
-        {
-            "day": 28,
-            "class": "disabled"
-        },
-        {
-            "day": 29,
-            "class": "disabled"
-        },
-        {
-            "day": 30,
-            "class": "disabled"
-        },
-        {
-            "day": 31,
-            "class": "disabled"
-        }
-    ]
+def get_items():
+    try:
+        with psycopg.connect(conninfo=config['conninfo']) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT content FROM gifts WHERE display_date <= CURRENT_DATE + 2 ORDER BY display_date ASC;")
+                x = cur.fetchall()
+        for i in range(len(x)):
+            x[i] = x[i][0]
+        [x.append({"day": d, "class": "upcoming"}) for d in range(len(x), 26)]
+        [x.append({"day": d, "class": "disabled"}) for d in range(26, 32)]
+        return x
+    except (Exception, psycopg.DatabaseError) as error:
+        raise error
